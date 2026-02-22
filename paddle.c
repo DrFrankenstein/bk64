84b5eac2d80a07b778b1636dd2b30927755b8f77
Carl Tessier <doktorfrankenstein@gmail.com>
2026-02-22 13:46:39 -0500

    refactor(paddle): MOAR CONSTANTS

--- paddle.c
+++ paddle.c
@@ -5,6 +5,12 @@
 #include "paddle.h"
 #include "sprite.h"
 
+#define PADDLE_WIDTH 24
+#define PADDLE_CENTER_OFF PADDLE_WIDTH / 24
+#define PADDLE_HEIGHT 5
+
+#define PADDLE_Y 230
+
 unsigned paddle_left(void)
 {
   return sprite_posx(SPR_PADDLE);
@@ -12,23 +18,23 @@ unsigned paddle_left(void)
 
 unsigned paddle_right(void)
 {
-  return paddle_left() + 24;
+  return paddle_left() + PADDLE_WIDTH;
 }
 
 unsigned paddle_pos(void)
 {
-  return paddle_left() + 12;
+  return paddle_left() + PADDLE_CENTER_OFF;
 }
 
 void paddle_move(unsigned pos)
 {
-  sprite_movex(SPR_PADDLE, pos - 12);
+  sprite_movex(SPR_PADDLE, pos - PADDLE_CENTER_OFF);
 }
 
 void paddle_show(void)
 {
   sprite_color(SPR_PADDLE, COLOR_WHITE);
-  sprite_movey(SPR_PADDLE, 230);
+  sprite_movey(SPR_PADDLE, PADDLE_Y);
   sprite_enable(SPR_PADDLE);
 }
 
